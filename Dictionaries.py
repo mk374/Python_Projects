f536cf7f86e0f29012a7f1e30aee57718f81a383
Min Soo Kim <mk374@duke.edu>
2018-09-10 18:15:10 -0400

    beginnings

--- Dictionaries.py
+++ Dictionaries.py
@@ -4,21 +4,17 @@ Created on Sep 10, 2018
 @author: minsookim
 '''
 from splinter import Browser
+browser = Browser('chrome',executable_path='/usr/local/bin/chromedriver')
 
-browser = Browser('chrome')
 
-with Browser() as browser:
-    # Visit URL
-    url = "http://www.google.com"
-    browser.visit(url)
-    browser.fill('q', 'splinter - python acceptance testing for web applications')
-    # Find and click the 'search' button
-    button = browser.find_by_name('btnG')
-    # Interact with elements
-    button.click()
-    if browser.is_text_present('splinter.readthedocs.io'):
-        print("Yes, the official website was found!")
-    else:
-        print("No, it wasn't found... We need to improve our SEO techniques")
-if __name__ == '__main__':
-    pass
+# Visit URL
+url = "http://www.jisho.org"
+browser.visit(url)
+
+browser.fill('keyword','あし\n')
+
+if browser.is_text_present('foot'):
+    print("Yes, the official website was found!")
+else:
+    print("No, it wasn't found... We need to improve our SEO techniques")
+browser.quit()
