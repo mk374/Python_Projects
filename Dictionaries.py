12cdd76054c95f44f80739e09d35c666303b9bee
Min Soo Kim <mk374@duke.edu>
2018-09-10 15:50:15 -0400

    inital comment

---
+++ Dictionaries.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 10, 2018
+
+@author: minsookim
+'''
+from splinter import Browser
+
+browser = Browser('chrome')
+
+with Browser() as browser:
+    # Visit URL
+    url = "http://www.google.com"
+    browser.visit(url)
+    browser.fill('q', 'splinter - python acceptance testing for web applications')
+    # Find and click the 'search' button
+    button = browser.find_by_name('btnG')
+    # Interact with elements
+    button.click()
+    if browser.is_text_present('splinter.readthedocs.io'):
+        print("Yes, the official website was found!")
+    else:
+        print("No, it wasn't found... We need to improve our SEO techniques")
+if __name__ == '__main__':
+    pass
